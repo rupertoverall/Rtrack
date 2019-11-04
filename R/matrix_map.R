61d78c42e8e42ee11164d1f214e247122eb24a2e
rupertoverall <mail@rupertoverall.net>
2019-11-04 10:51:58 +0100

    Not required.

--- R/matrix_map.R
+++
@@ -1,8 +0,0 @@
-matrix.map = function(n, dim.m){
-  col = ceiling(n / dim.m[1])
-  row = n - ((col - 1) * dim.m[1])
-  outofbounds = col < 1 | col > dim.m[2] | row < 1 | row > dim.m[1]
-  col[outofbounds] = NA
-  row[outofbounds] = NA
-  return(list(row=row, col=col))
-}
