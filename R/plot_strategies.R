73ddfc7100d0111117ad47730132546d07ddf6de
rupertoverall <35666678+rupertoverall@users.noreply.github.com>
2025-08-11 20:12:30 +0200

    Improved handling of boundaries and extra plotting parameters.

--- R/plot_strategies.R
+++ R/plot_strategies.R
@@ -30,18 +30,37 @@
 #' @param factor The factor by which the data should be grouped. The default,
 #'   \code{NA}, means that all data will be shown in one plot. Specifying a
 #'   grouping factor will cause a separate plot to be generated for each group.
+#' @param x.axis The scale of the x axis. The default, "Day", will add a
+#'   labelled axis with tick marks at each day. If this parameter is set to
+#'   "Trial", then tick marks are added for each trial. If set to "none", then
+#'   no x axis will be drawn.
 #' @param exclude.probe Should data from probe trials be excluded (see Details).
 #' @param boundaries Where should the boundaries between arena types be drawn
 #'   (see Details).
+#' @param boundary.lwd The thickness of the boundaries. Default is 1.
 #' @param legend Should a legend be drawn. Default is to add a legend to the
 #'   plot.
 #' @param screen Should multiple plots be drawn to one page. Default is
 #'   \code{FALSE}. This can be useful for advanced layout using
 #'   \code{\link[graphics]{split.screen}}.
+#' @param layout If \code{screen} is \code{TRUE}, this parameter can be given as
+#'   a matrix of indices or level names. The multiple sub-plots will be drawn in
+#'   a grid in the order and layout specified.
+#' @param title Text for the plot title. The default includes the name of the
+#'   factor and the level encoded as the wildcard "%%f". The remaining text can
+#'   be replaced (for example for language localisation). Using the string "%%f"
+#'   on its own will print the factor and level without additional text. The
+#'   string "" will suppress the title entirely. Note that this overrides the
+#'   usual plotting parameter 'main' (which would give the same title to all
+#'   subplots).
+#' @param axis.titles Should axis titles be drawn. Default is to add titles for
+#'   the x and y axes. These can be suppressed and added afterwards (using
+#'   \code{\link[graphics]{title}}). This might be helpful for localising to a
+#'   different language for example.
 #' @param margins The margins of the plot (see the option \code{mar} in
 #'   \code{\link[graphics]{par}}). The defaults should usually be fine, but they
 #'   can be overridden if, for example, factor names are very long.
-#' @param lwd The thickness of the plotted lines. Default is 2.
+#' @param ... Additional arguments to \code{plot}.
 #'
 #' @return A \code{\link[base]{list}} of strategy call information.
 #'
@@ -57,16 +76,14 @@
 #' @importFrom stats aggregate
 #'
 #' @export
-plot_strategies = function(strategies, experiment, factor = NA, exclude.probe = FALSE, boundaries = NA, legend = TRUE, screen = FALSE, margins = c(5, 4, 4, 8), lwd = 2){
+plot_strategies = function(strategies, experiment, factor = NA, x.axis = "day", exclude.probe = FALSE, boundaries = NA, boundary.lwd = 1, legend = TRUE, screen = FALSE, layout = NULL, title = "Strategy usage for %%f", axis.titles = TRUE, margins = c(5, 4, 4, 8), ...){
 	if(methods::is(strategies, "rtrack_strategies") & methods::is(experiment, "rtrack_experiment")){
-		boundary.lwd = lwd
-
 		plotting.data = experiment$factors
 		if(exclude.probe){
 			if(!is.null(plotting.data$Probe) & all(plotting.data$Probe %in% c("TRUE", "FALSE"))){
 				plotting.data$y[plotting.data$Probe == "TRUE"] = NA
 			}else{
-				warning("There is no 'Probe' factor in the experiment (or it contains values other that TRUE/FALSE). All data will be plotted instead.")
+				warning("There is no 'Probe' factor in the experiment (or it contains values other than TRUE/FALSE). All data will be plotted instead.")
 			}
 		}
 		plotting.data$`_Day` = as.numeric(plotting.data$`_Day`)
@@ -91,6 +108,19 @@ plot_strategies = function(strategies, experiment, factor = NA, exclude.probe = 
 			plot.series = list("All data" = 1:nrow(experiment$factors))
 		}
 
+		parameters = list(...)
+		lwd = as.numeric(parameters[which(names(parameters) == "lwd")])
+		if(length(lwd) == 0 ) lwd = 1
+		bty = parameters[which(names(parameters) == "bty")]
+		if(length(bty) == 0 ) bty = "o"
+		las = as.numeric(parameters[which(names(parameters) == "las")])
+		if(length(las) == 0 ) las = 2
+		maintitle = as.numeric(parameters[which(names(parameters) == "main")])
+		if(length(maintitle) > 0 ){
+			message("Use 'title' instead of 'main' to allow titling of multiple plots.") 
+			parameters$main = NULL
+		}
+		
 		arena = as.character(aggregate(plotting.data$`_Arena`, list(plotting.data$`_Trial`, plotting.data$`_Day`), `[`, 1)$x)
 		# If boundaries not user-set, then place at the interface of different arenas
 		if(is.null(boundaries)){
@@ -101,51 +131,88 @@ plot_strategies = function(strategies, experiment, factor = NA, exclude.probe = 
 			all.trials = unique(plotting.data[, c("_Day", "_Trial")])
 			boundaries = apply(boundaries, 1, function(row) which(all.trials$`_Day` == as.numeric(row[1]) & all.trials$`_Trial` == as.numeric(row[2])) )
 		}
-		# plotting.data is ordered, so put day marker at first trial of the day (might not be trial 1 if there were experimental dropouts)
+		# Plotting.data is ordered, so put day marker at first trial of the day (might not be trial 1 if there were experimental dropouts)
 		day = match(unique(plotting.data$`_Day`), unique(plotting.data[, c("_Trial", "_Day")])$`_Day`)
-
+		trial = seq_along(unique(plotting.data[, c("_Trial", "_Day")])$`_Trial`)
+		
+		indices = seq_along(plot.levels)
+		screens = NULL
 		if(screen){
 			n = length(plot.levels)
-			screen.layout = t(sapply(1:n, function(m){ c(0, 1, m / n - 1 / n, m / n) }))
-			screens = graphics::split.screen(screen.layout)
+			# Check if there is a user-defined layout - if not, use default.
+			if(is.null(layout)){
+				screens = graphics::split.screen(figs = c(length(plot.levels), 1))
+			}else{
+				if(length(layout) == 1) layout = as.matrix(layout)
+				# Convert to byRow format.
+				layout = matrix(as.vector(t(layout)), byrow = FALSE, ncol = ncol(layout))
+				# Sanity check.
+				indices = suppressWarnings(as.numeric(layout))
+				if(length(na.omit(indices)) == 0){
+					indices = match(as.character(layout), plot.levels) # Match to level names.
+				}
+				sane = all(is.matrix(layout), length(indices) > 0, all(na.omit(indices) %in% seq_len(n)))
+				if(!sane) stop("The 'layout' parameter must be a matrix with indices/elements corresponding to the factor levels.")
+				screens = graphics::split.screen(figs = c(nrow(layout), ncol(layout)))
+			}
 		}
-		plot.values = sapply(seq_along(names(plot.series)), function(i){
-			level = names(plot.series)[i]
-			group = plotting.data[plot.series[[level]], ]
-			strategy.ids = as.numeric(strategies$calls[rownames(group), ]$strategy)
-			x = unique(group$x)
-	
-			# Reversed, ordered, cumulative counts - ready for plotting
-			y.cum = do.call("rbind", by(strategy.ids, list(group$`_Trial`, group$`_Day`), function(y){
-				rev(cumsum(sapply(rev(strategies$plot.order), function(s) length(which(y == s)) )))
-			}))
-			plot.colours = strategies$strategy.colours[strategies$plot.order]
-			
+		plot.values = sapply(seq_along(indices), function(i){
 			if(screen) graphics::screen(screens[i])
 			.parprevious = graphics::par(mar = margins, xpd = TRUE)
 			on.exit(par(.parprevious))
-
-			plot(x, seq(0, max(y.cum), length.out = length(x)), type = "n", las = 1, xaxt = "n", xaxs="i", yaxs="i", ylab = "Strategy usage", xlab = "Day")
-			for(n in 1:ncol(y.cum)){
-				polygon(c(x[1], x, x[length(x)]), c(0, y.cum[, n], 0), col = plot.colours[n], border = NA)
+			
+			if(!is.na(indices[i])){
+				level = names(plot.series)[indices[i]]
+				group = plotting.data[plot.series[[level]], ]
+				strategy.ids = as.numeric(strategies$calls[rownames(group), ]$strategy)
+				x = unique(group$x)
+		
+				# Reversed, ordered, cumulative counts - ready for plotting
+				y.cum = do.call("rbind", by(strategy.ids, list(group$`_Trial`, group$`_Day`), function(y){
+					rev(cumsum(sapply(rev(strategies$plot.order), function(s) length(which(y == s)) )))
+				}))
+				plot.colours = strategies$strategy.colours[strategies$plot.order]
+				
+				do.call(plot, c(list(x, seq(0, max(y.cum), length.out = length(x)), type = "n", las = 1, xaxt = "n", xaxs = "i", yaxs = "i", ylab = "", xlab = "", main = ""), parameters))
+				for(n in 1:ncol(y.cum)){
+					polygon(c(x[1], x, x[length(x)]), c(0, y.cum[, n], 0), col = plot.colours[n], border = NA)
+				}
+				if(exclude.probe & !is.null(plotting.data$Probe)){
+					probe = which(aggregate(plotting.data$Probe, list(plotting.data$`_Trial`, plotting.data$`_Day`), `[`, 1)$x == "TRUE")
+					rect(probe, 0, probe - 1, max(y.cum), border = NA, col = "#FFFFFFFF")
+				}
+				if(!is.null(boundaries) & length(boundaries) > 0){
+					rect(boundaries, 0, boundaries - 1, max(y.cum), border = NA, col = "#FFFFFFFF")
+					segments(boundaries - 0.5, 0, boundaries - 0.5, max(y.cum), lty = 3, lwd = boundary.lwd, col = "#000000FF")
+				}
+				if(bty != "n") graphics::box(lwd = lwd)
+				
+				if(length(plot.series) > 1){
+					factor.text = paste0(tolower(factor), " '", level, "'")
+					title(main = sub("\\%\\%f", factor.text, title))
+				}else{
+					factor.text = "all data"
+					title(main = sub("\\%\\%f", factor.text, title))
+				}
+				
+				# Plotting.data is ordered, so put day marker at first trial of the day (might not be trial 1 if there were experimental dropouts)
+				if(tolower(x.axis) == "day"){
+					axis(1, at = day, labels = 1:length(day))
+				}else if(tolower(x.axis) == "trial"){
+					axis(1, at = trial, labels = 1:length(trial))
+				}
+				
+				if(axis.titles){
+					if(tolower(x.axis) == "day"){
+						title(xlab = "Day", ylab = "Strategy usage")
+					}else if(tolower(x.axis) == "trial"){
+						title(xlab = "Trial", ylab = "Strategy usage")
+					}
+				}
+			
+				if(legend) graphics::legend(x = nrow(y.cum), y = max(y.cum), legend = rev(strategies$strategy.names), fill = rev(strategies$strategy.colours), cex = .7, bty = "n", border = NA)
 			}
-			if(exclude.probe & !is.null(plotting.data$Probe)){
-				probe = which(aggregate(plotting.data$Probe, list(plotting.data$`_Trial`, plotting.data$`_Day`), `[`, 1)$x == "TRUE")
-				rect(probe, 0, probe - 1, max(y.cum), border = NA, col = "#FFFFFFFF")
-			}
-			if(!is.null(boundaries) & length(boundaries) > 0){
-				rect(boundaries, 0, boundaries - 1, max(y.cum), border = NA, col = "#FFFFFFFF")
-				segments(boundaries - 0.5, 0, boundaries - 0.5, max(y.cum), lty = 3, lwd = boundary.lwd, col = "#000000FF")
-			}
-			box(lwd = boundary.lwd)
-			axis(1, at = day, labels = 1:length(day))
-			if(length(plot.series) > 1){
-				title(main = paste0("Strategy usage for ", tolower(factor), " '", level, "'"))
-			}else{
-				title(main = paste0("Strategy usage for all data"))
-			}
-			if(legend) graphics::legend(x = nrow(y.cum), y = max(y.cum), legend = rev(strategies$strategy.names), fill = rev(strategies$strategy.colours), cex = .7, bty = "n", border = NA)
-		return(NULL)
+			return(NULL)
 		})
 		graphics::close.screen(screens)
 	}
