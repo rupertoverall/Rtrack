7fe867ce28fb50cd1f60fe4f7b2fb2200b54edea
rupertoverall <35666678+rupertoverall@users.noreply.github.com>
2025-08-11 20:26:14 +0200

    Better detection of generic text formats.

--- R/identify_track_format.R
+++ R/identify_track_format.R
@@ -64,9 +64,9 @@ identify_track_format = function(filename = NULL) {
 		invisible(supported.formats)
 	}else{
 		# Don't rely on the file extension - it might be wrong.
-		# Test for each format agnostically
-		testlines = 100 # The number of lines to be tested (make sure this many lines exist)
-		headerlines = 50 # The number of testlines that are allowed to contain non-tabular header information
+		# Test for each format agnostically.
+		testlines = 100 # The number of lines to be tested (make sure this many lines exist).
+		#headerlines = 50 # The number of testlines that are allowed to contain non-tabular header information
 		raw = tryCatch({suppressMessages(as.data.frame(readxl::read_excel(filename, col_names = FALSE), stringsAsFactors = FALSE))}, error = function(e){FALSE})
 		if(length(dim(raw)) > 0){   
 			# Looks like a valid Excel document
@@ -102,17 +102,21 @@ identify_track_format = function(filename = NULL) {
 				raw = as.matrix(suppressMessages(utils::read.table(filename, nrows = testlines, sep = "\n", fill = TRUE, header = FALSE, stringsAsFactors = FALSE, fileEncoding = encoding)))
 			})
 		
-			# Try reading the standard tabular types (from least to most common non-separator characters)
-			# Allow for up to 'headerlines' header lines (there is no way to predict this beforehand)
-			tsv.cols = table(sapply(raw[headerlines:testlines], function(s) length(which(gregexpr("\t", s)[[1]] > 0)) + 1 )) 
-			csv2.cols = table(sapply(raw[headerlines:testlines], function(s) length(which(gregexpr("\\;", s)[[1]] > 0)) + 1 ))
-			csv.cols = table(sapply(raw[headerlines:testlines], function(s) length(which(gregexpr("\\,", s)[[1]] > 0)) + 1 ))
-			posix.points = table(sapply(raw[headerlines:testlines], function(s) length(which(gregexpr("[0-9]\\:[0-9]", s)[[1]] > 0)) + 1 ))
-				
+			# Try reading the standard tabular types.
+			testlines = min(nrow(raw), testlines)
+			tsv.cols = table(sapply(raw[0:testlines], function(s) length(which(gregexpr("\t", s)[[1]] > 0)) + 1 )) 
+			tsv.cols = sort(tsv.cols, decreasing = TRUE)[1]
+			csv.cols = table(sapply(raw[0:testlines], function(s) length(which(gregexpr("\\,", s)[[1]] > 0)) + 1 ))
+			csv.cols = sort(csv.cols, decreasing = TRUE)[1]
+			csv2.cols = table(sapply(raw[0:testlines], function(s) length(which(gregexpr("\\;", s)[[1]] > 0)) + 1 ))
+			csv2.cols = sort(csv2.cols, decreasing = TRUE)[1]
+			posix.points = table(sapply(raw[0:testlines], function(s) length(which(gregexpr("[0-9]\\:[0-9]", s)[[1]] > 0)) + 1 ))
+			posix.points = sort(posix.points, decreasing = TRUE)[1]
+			
 			if(length(csv.cols) == 1 & as.numeric(names(posix.points)) > 2){ # Anymaze format (with Posix timestamps)
 				# Has tabular data that is tab-separated (with or without headers)
 				# Is this a headerless table of numeric data?
-				raw = utils::read.csv(filename, header = T, row.names = NULL, stringsAsFactors = FALSE, fileEncoding = encoding)
+				raw = utils::read.csv(filename, header = TRUE, row.names = NULL, stringsAsFactors = FALSE, fileEncoding = encoding)
 				raw$Time = suppressWarnings(as.numeric(strptime(raw$Time, format = "%H:%M:%OS")))
 				if(is.numeric(raw$Time) & is.numeric(raw$Centre.position.X) & is.numeric(raw$Centre.position.Y)){
 					track.format = "anymaze.csv"
@@ -120,44 +124,51 @@ identify_track_format = function(filename = NULL) {
 			}else if(length(tsv.cols) == 1 & as.numeric(names(tsv.cols)) > 2){ # Minimum 3 columns required for t, x, y
 				# Has tabular data that is tab-separated (with or without headers)
 				# Is this a headerless table of numeric data?
-				raw = utils::read.delim(filename, header = F, stringsAsFactors = FALSE, fill = T, fileEncoding = encoding)
-				numtest.all = all(apply(raw[, 1:3], 1, function(row) all(grepl("[0-9\\-\\.\\NA\\ ]+", row) | nchar(row) == 0 | is.na(row)) ))
+				raw = utils::read.delim(filename, header = FALSE, stringsAsFactors = FALSE, fill = T, fileEncoding = encoding)
+				numtest.all = FALSE
+				if(ncol(raw) >= 3){ # Catch crazy formats with headers.
+					numtest.all = all(apply(raw[, 1:3], 1, function(row) all(grepl("[0-9\\-\\.\\NA\\ ]+", row) | nchar(row) == 0 | is.na(row)) ))
+				}else{
+					numtest.all = FALSE
+				}
 				if(any(grepl("%%BEGIN_HEADER", raw))  & any(grepl("Tracker version 2", raw))){
 					track.format = "tracker.2.dat"
 				}else if(ncol(raw) >= 3 & numtest.all){
 					track.format = "raw.nh.tab"
+				}else if(FALSE){
+					# TODO Add an extra test for Topscan here: the format has variants that can trigger this block.
 				}else{
-					raw = utils::read.delim(filename, header = T, stringsAsFactors = FALSE, fill = T, fileEncoding = encoding)
+					raw = utils::read.delim(filename, header = TRUE, stringsAsFactors = FALSE, fill = T, fileEncoding = encoding)
 					numtest.all = all(apply(raw[, 1:3], 1, function(row) all(grepl("[0-9\\-\\.\\NA\\ ]+", row) | nchar(row) == 0 | is.na(row)) ))
 					if(ncol(raw) == 3 & numtest.all){
 						track.format = "raw.tab"
-					}else if(ncol(raw) >= 3 & numtest.all){
-						track.format = "raw.free.tab"
-						message("This format has more than 3 columns. Please make sure that the _first_3_ are 'time', 'x', 'y' in that order.")
+					#}else if(ncol(raw) >= 3 & numtest.all){
+					#	track.format = "raw.free.tab"
+					#	message("This format has more than 3 columns. Please make sure that the _first_3_ are 'time', 'x', 'y' in that order.")
 					}
 				}
 			}else if(length(csv2.cols) == 1 & as.numeric(names(csv2.cols)) > 2){ 
 				# Has tabular data that is semicolon-separated (with or without headers)
-				# NOTE that Ethovison XT can use variously "Header Lines:" of "Number of header Lines:", so check for both.
+				# NOTE that Ethovison XT can use variously "Header Lines:" or "Number of header Lines:", so check for both.
 				if(grepl("header\\ lines", raw[1], ignore.case = T) & any(grepl("Experiment|Trial\\ name|Trial\\ ID|Arena\\ name", raw[1:20,]))){
 					track.format = "ethovision.xt.csv2"
 				}else if(any(grepl("Track\\ file|Object|Samples|Goal\\ Position", raw[1:20,]))){
 					track.format = "ethovision.3.csv2"
 				}else{
 					# Is this a headerless table of numeric data?
-					raw = utils::read.csv2(filename, header = F, row.names = NULL, stringsAsFactors = FALSE, fileEncoding = encoding)
+					raw = utils::read.csv2(filename, header = FALSE, row.names = NULL, stringsAsFactors = FALSE, fileEncoding = encoding)
 					numtest.all = all(apply(raw[, 1:3], 1, function(row) all(grepl("[0-9\\-\\.\\NA\\ ]+", row) | nchar(row) == 0 | is.na(row)) ))
 					if(numtest.all){
 						track.format = "raw.nh.csv"
 					}else{
-						raw = utils::read.csv2(filename, header = T, row.names = NULL, stringsAsFactors = FALSE, fileEncoding = encoding)
+						raw = utils::read.csv2(filename, header = TRUE, row.names = NULL, stringsAsFactors = FALSE, fileEncoding = encoding)
 						numtest.all = all(apply(raw, 1, function(row) all(grepl("[0-9\\-\\.\\NA\\ ]+", row) | nchar(row) == 0 | is.na(row)) ))
 						numtest.sans = all(apply(raw[-1, ], 1, function(row) all(grepl("[0-9\\-\\.\\NA\\ ]+", row) | nchar(row) == 0 | is.na(row)) ))
 						if(ncol(raw) == 3 & numtest.all){
 							track.format = "raw.csv2"
-						}else if(ncol(raw) >= 3 & numtest.all){
-							track.format = "raw.free.csv2"
-							message("This format has more than 3 columns. Please make sure that the _first_3_ are 'time', 'x', 'y' in that order.")
+						#}else if(ncol(raw) >= 3 & numtest.all){
+						#	track.format = "raw.free.csv2"
+						#	message("This format has more than 3 columns. Please make sure that the _first_3_ are 'time', 'x', 'y' in that order.")
 						}else if(ncol(raw) %% 3 == 0 & numtest.sans){
 							track.format = "actimetrics.watermaze.csv2"
 						}
@@ -165,26 +176,26 @@ identify_track_format = function(filename = NULL) {
 				}
 			}else if(length(csv.cols) == 1 & as.numeric(names(csv.cols)) > 2){ 
 				# Has tabular data that is comma-separated (with or without headers)
-				# NOTE that Ethovison XT can use variously "Header Lines:" of "Number of header Lines:", so check for both.
+				# NOTE that Ethovison XT can use variously "Header Lines:" or "Number of header Lines:", so check for both.
 				if(grepl("header\\ lines", raw[1], ignore.case = T) & any(grepl("Experiment|Trial\\ name|Trial\\ ID|Arena\\ name", raw[1:20]))){
 					track.format = "ethovision.xt.csv"
 				}else if(any(grepl("Track\\ file|Object|Samples|Goal\\ Position", raw[1:20]))){
 					track.format = "ethovision.3.csv"
 				}else{
 					# Is this a headerless table of numeric data?
-					raw = utils::read.csv(filename, header = F, row.names = NULL, stringsAsFactors = FALSE, fileEncoding = encoding)
+					raw = utils::read.csv(filename, header = FALSE, row.names = NULL, stringsAsFactors = FALSE, fileEncoding = encoding)
 					numtest.all = all(apply(raw[, 1:3], 1, function(row) all(grepl("[0-9\\-\\.\\NA\\ ]+", row) | nchar(row) == 0 | is.na(row)) ))
 					if(numtest.all){
 						track.format = "raw.nh.csv"
 					}else{
-						raw = utils::read.csv(filename, header = T, row.names = NULL, stringsAsFactors = FALSE, fileEncoding = encoding)
+						raw = utils::read.csv(filename, header = TRUE, row.names = NULL, stringsAsFactors = FALSE, fileEncoding = encoding)
 						numtest.all = all(apply(raw, 1, function(row) all(grepl("[0-9\\-\\.\\NA\\ ]+", row) | nchar(row) == 0 | is.na(row)) ))
 						numtest.sans = all(apply(raw[-1, ], 1, function(row) all(grepl("[0-9\\-\\.\\NA\\ ]+", row) | nchar(row) == 0 | is.na(row)) ))
 						if(ncol(raw) == 3 & numtest.all){
 							track.format = "raw.csv"
-						}else if(ncol(raw) >= 3 & numtest.all){
-							track.format = "raw.free.csv"
-							message("This format has more than 3 columns. Please make sure that the _first_3_ are 'time', 'x', 'y' in that order.")
+						#}else if(ncol(raw) >= 3 & numtest.all){
+						#	track.format = "raw.free.csv"
+						#	message("This format has more than 3 columns. Please make sure that the _first_3_ are 'time', 'x', 'y' in that order.")
 						}else if(ncol(raw) %% 3 == 0 & numtest.sans){
 							track.format = "actimetrics.watermaze.csv"
 						}
